468abc60d8cd11b4b21338aafea1ad2b28351b37
Shibani Mukherjee <shibani07mukherjee@gmail.com>
2026-07-21 14:38:14 +0530

    login system

---
+++ miniprij4.py
@@ -0,0 +1,6 @@
+username= input("Enter Username : ")
+password= input("Enter Password : ")
+if username == "hini" and password == "1234":
+    print("WELCOME MY DIVAAA")
+else:
+    print("Invalid Username or Password")
